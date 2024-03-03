d0ef49f359665e8a4f2184bef4cf6a6d041b53e7
seanygberg <60160094+seanygberg@users.noreply.github.com>
2024-03-03 18:47:06 -0500

    Added some tests

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -18,4 +18,30 @@ it("should return a result with 2 decimal places", function() {
   expect(calculateMonthlyPayment(values)).toEqual('1.60');
 });
 
-/// etc
+it("should handle no amount", function() {
+  const values = {
+    amount: 0, 
+    years: 8, 
+    rate: 5.8
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('0.00');
+});
+
+it("should handle no years", function() {
+  const values = {
+    amount: 10, 
+    years: 0, 
+    rate: 5.8
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('Infinity');
+});
+
+it("should handle no rate", function() {
+  const values = {
+    amount: 10, 
+    years: 8, 
+    rate: 0
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('NaN');
+});
+
