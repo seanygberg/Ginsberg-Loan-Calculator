2cc315965a07c119aff13db3642b6352b4de2e45
seanygberg <60160094+seanygberg@users.noreply.github.com>
2024-03-02 23:50:31 -0500

    Calculator is functionable, will add more tests

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,21 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = {
+    amount: 100000, 
+    years: 30, 
+    rate: 5
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('536.82');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const values = {
+    amount: 123, 
+    years: 8, 
+    rate: 5.8
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('1.60');
 });
 
 /// etc
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,20 +21,38 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const values = {amount: 100000, years: 30, rate: 5}
+  const amountInput = document.getElementById("loan-amount");
+  amountInput.value = values.amount;
+  const yearsInput = document.getElementById("loan-years");
+  yearsInput.value = values.years;
+  const rateInput = document.getElementById("loan-rate");
+  rateInput.value = values.rate;
+  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const currentValues = getCurrentUIValues();
+  updateMonthly(calculateMonthlyPayment(currentValues));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const p = values.amount;
+  const i = (values.rate / 100) / 12;
+  const n = values.years * 12;
+  const numerator = p * i;
+  const denominator = 1 - Math.pow((1 + i), -n);
+  return (numerator / denominator).toFixed(2);
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const monthlyInput = document.getElementById("monthly-payment");
+  monthlyInput.innerText = "$" + monthly;
 }
