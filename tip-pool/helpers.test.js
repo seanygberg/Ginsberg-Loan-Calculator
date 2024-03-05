25b51cefdcdaacc0af84604828235e2439fea161
seanygberg <60160094+seanygberg@users.noreply.github.com>
2024-03-04 22:52:58 -0500

    helpers.test is done

--- tip-pool/helpers.test.js
+++ tip-pool/helpers.test.js
@@ -3,7 +3,7 @@ describe("Helpers test (with setup and tear-down)", function() {
         billAmtInput.value = 50;
         tipAmtInput.value = 10;
         submitPaymentInfo();
-    })
+    });
 
     it ("should sum total tip", function() {
         expect(sumPaymentTotal('tipAmt')).toEqual(10);
@@ -11,7 +11,7 @@ describe("Helpers test (with setup and tear-down)", function() {
         tipAmtInput.value = 10;
         submitPaymentInfo();
         expect(sumPaymentTotal('tipAmt')).toEqual(20);
-    })
+    });
 
     it ("should sum total bill", function() {
         expect(sumPaymentTotal('billAmt')).toEqual(50);
@@ -19,7 +19,24 @@ describe("Helpers test (with setup and tear-down)", function() {
         tipAmtInput.value = 10;
         submitPaymentInfo();
         expect(sumPaymentTotal('billAmt')).toEqual(100);
-    })
+    });
+
+    it ("should calculate tip percentage", function() {
+        expect(calculateTipPercent(100, 15)).toEqual(15);
+        expect(calculateTipPercent(10, 15)).toEqual(150);
+    });
+
+    it ("should add new td to the table", function() {
+        let myTr = document.createElement('tr');
+        
+        appendTd(myTr, "first");
+        appendTd(myTr, "second");
+        
+        expect(myTr.children.length).toEqual(2);
+        expect(myTr.firstChild.innerHTML).toEqual('first');
+        expect(myTr.firstChild.nextSibling.innerHTML).toEqual('second');
+
+    });
 
     afterEach(function() {
         billAmtInput.value = 0;
